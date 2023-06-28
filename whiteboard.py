91a086593cfd6bab773f0bb26a0fbe772db6a8d1
kevin beier <kevinb@codingtemple.com>
2023-06-28 15:45:37 -0400

    Good Luck

---
+++ whiteboard.py
@@ -0,0 +1,8 @@
+# Write a function that takes an integer flight_length (in minutes) 
+# and a list of integers movie_lengths (in minutes) 
+# and returns a boolean indicating whether there are exactly two movies
+# that would take the entire length of the flight
+# Draw more insight about how to structure this function from reading the test file
+
+# Write your solution Below:
+
